f18b3586dbf8880afb909ad867e2c67af016c3f7
XavierL2305 <xavilahur@gmail.com>
2026-06-18 18:05:15 -0400

    Se agregaron los estilos a los botones y los terminos de servicio minuto 52:38

--- app/(app)/(publico)/index.tsx
+++ app/(app)/(publico)/index.tsx
@@ -1,11 +1,14 @@
 import GoogleAutenBoton from "@/componentes/auten/GoogleAutenBoton";
 import { Fonts } from "@/constants/theme";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Animated, { FadeInDown } from "react-native-reanimated";
 
 // Pantalla de bienvenida con animación de fade-in para el logo y el texto
 
 export default function Index() {
+  const openWebBrowser = () => {
+    Linking.openURL('https://galaxies.dev')
+  }
   return (
     <View style={styles.container}>
       <View style={styles.infiniteScrollContainer}></View>
@@ -26,12 +29,25 @@ export default function Index() {
             <GoogleAutenBoton />
           </Animated.View>
           <Animated.View entering={FadeInDown.delay(200)}>
-            <TouchableOpacity style={styles.otherButton} />
-            <Text style={styles.otherButtonText}>
-              Otro método de autenticación
-            </Text>
+            <TouchableOpacity style={styles.otherButton} >
+              <Text style={styles.otherButtonText}>
+                Otro método de autenticación
+              </Text>
+            </TouchableOpacity>
           </Animated.View>
         </View>
+
+        <Animated.View style={styles.privacyContainer} entering={FadeInDown.delay(400)}>
+          <Text style={styles.privacyText}>
+            Por favor visita{" "}
+            <Text style={styles.privacyLink} onPress = {openWebBrowser}>
+              Términos de Servicio
+            </Text> y{" "}
+            <Text style={styles.privacyLink}>
+              Política de Privacidad
+            </Text>
+          </Text>
+        </Animated.View>
       </View>
     </View>
   );
@@ -44,6 +60,8 @@ const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
     alignItems: "center",
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
   brandLogo: {
     width: "100%",
@@ -63,8 +81,37 @@ const styles = StyleSheet.create({
     gap: 12,
     width: "100%",
   },
-  otherButton: {},
-  otherButtonText: {},
+  otherButton: {
+    backgroundColor: "#f0f0f0",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 17,
+    borderRadius: 12,
+    gap: 4,
+  },
+  otherButtonText: {
+    color: "#666",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+
+  privacyContainer: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  privacyText: {
+    fontSize: 12,
+    color: "#999",
+    textAlign: "center",
+    lineHeight: 16,
+  },
+  privacyLink: {
+    color: "#4285F4",
+    textDecorationLine: "underline",
+  },
+
+
   infiniteScrollContainer: {
     flex: 0.5,
   },
