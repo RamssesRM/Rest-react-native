9571c52a3dd1e3b7bf692ebf783288c7bb3efb85
RamssesRM <isram0717@gmail.com>
2026-06-25 02:07:31 -0400

    Fix: resolviendo conflicto de fusion en ComidasList

--- componentes/ComidasList.tsx
+++ componentes/ComidasList.tsx
@@ -1,38 +1,47 @@
-import { Colors } from '@/constants/theme';
-import { useRestaurants } from '@/hooks/useRestaurants';
-import React from 'react';
-import { ActivityIndicator, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Colors } from "@/constants/theme";
+import { useRestaurants } from "@/hooks/useRestaurants";
+import React from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const ComidasList = () => {
-  const { data:restaurants , isLoading, error } = useRestaurants();
+  const { data: restaurants, isLoading, error } = useRestaurants();
 
-  if (isLoading){
-    return(
-      <View>
-        <ActivityIndicator size={'small'} color={Colors.secondary}/>
+  if (isLoading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size={"large"} color={Colors.secondary} />
       </View>
-    )
+    );
   }
 
-
   return (
-    <View>
-      {restaurants?.map((item)=>(
+    <View style={styles.container}>
+      {restaurants?.map((item) => (
         <View key={item.id}>
           <TouchableOpacity style={styles.card} />
         </View>
       ))}
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1
+  container: {
+    flex: 1,
   },
-  card:{
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    // Aquí irán los estilos de tus tarjetas de comida
+  },
+});
 
-  }
-})
-
-export default ComidasList
+export default ComidasList;
