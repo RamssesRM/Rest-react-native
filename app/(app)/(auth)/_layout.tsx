b72e8526918863c043a808fcc7385499ad2beddd
RamssesRM <isram0717@gmail.com>
2026-07-08 01:28:01 -0400

    Creacion de los datos de Ubicacion

--- app/(app)/(auth)/_layout.tsx
+++ app/(app)/(auth)/_layout.tsx
@@ -1,10 +1,38 @@
-import { Stack } from "expo-router";
+import { Colors } from "@/constants/theme";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack, useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native";
 
 const Layout = () => {
+  const router = useRouter();
   return (
     <Stack screenOptions={{ headerShown: false }}>
       {/* Cambiado de 'index' a '(tabs)' */}
       <Stack.Screen name="(tabs)" />
+      <Stack.Screen
+        name="(modal)/location"
+        options={{
+          presentation: "modal",
+          animation: "slide_from_bottom",
+          sheetAllowedDetents: [0.7],
+          title: "Ubicacion",
+          headerShadowVisible: false,
+          sheetCornerRadius: 16,
+          sheetGrabberVisible: true,
+          headerRight: () => (
+            <TouchableOpacity
+              style={{
+                padding: 6,
+                borderRadius: 20,
+                backgroundColor: Colors.light,
+              }}
+              onPress={() => router.dismiss()}
+            >
+              <Ionicons name="close-sharp" size={24} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Stack>
   );
 };
--- componentes/ComidasHeader.tsx
+++ componentes/ComidasHeader.tsx
@@ -1,65 +1,135 @@
-import { Ionicons } from '@expo/vector-icons';
-import { View, Text, StyleSheet, TouchableOpacity, } from 'react-native'
-import Animated, { SharedValue } from 'react-native-reanimated';
 import { Colors } from "@/constants/theme";
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View, } from "react-native";
+import Animated, { SharedValue } from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 interface ComidasHeaderProps {
-    title: string;
-    scrollOffset: SharedValue<number>;
+  title: string;
+  scrollOffset: SharedValue<number>;
 }
 
 const ComidasHeader = ({ title, scrollOffset }: ComidasHeaderProps) => {
   const insets = useSafeAreaInsets();
   return (
     <Animated.View style={[styles.headerContainer, { paddingTop: insets.top }]}>
+      {/* header 1*/}
       <Animated.View style={[styles.header1]}>
+        <Link href={'/(app)/(auth)/(modal)/location'} asChild>
+          <TouchableOpacity style={styles.locationButton}>
+            <View style={styles.locationButtonIcon}>
+              <Ionicons name='business-outline'size={16} />
+            </View>
+            <Text style={styles.locationText}>Ubicación</Text>
+            <Ionicons name='chevron-down'size={16} />
+          </TouchableOpacity>
+        </Link>
 
-    <TouchableOpacity style={styles.locationButton}>
-      <View style={styles.locationButtonIcon}>
-        <Ionicons name='business-outline'size={16} />
+        <View style={styles.rightIcons}>
+          <TouchableOpacity style={styles.iconButton}>
+            <Ionicons name='filter' size={20} />
+          </TouchableOpacity>
+        <TouchableOpacity style={styles.iconButton}>
+          <Ionicons name='map-outline' size={20} />
+        </TouchableOpacity>
       </View>
-      <Text style={styles.locationText}>Ubicación</Text>
-      <Ionicons name='chevron-down'size={16} />
-    </TouchableOpacity>
-    <View style={styles.rightIcons}>
-      <TouchableOpacity style={styles.iconButton}>
-        <Ionicons name='filter' size={20} />
-      </TouchableOpacity>
-    </View>
-      </Animated.View>
+      </Animated.View> 
+
+      {/* Header 2 */}
+      {/* <Animated.View style={[styles.header2]}>
+        <View style={styles.centerContent}>
+          <Text style={styles.titleSmall}>{title}</Text>
+          <TouchableOpacity style={styles.locationSmall}>
+            <Text style={styles.locationSmallTextt}>Ubicación</Text>
+            <Ionicons name="chevron-down" size={14} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.rightIcons}>
+          <TouchableOpacity style={styles.iconButton}>
+            <Ionicons name="filter" size={20} />
+          </TouchableOpacity>
+        </View>
+      </Animated.View> */}
     </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
-    headerContainer: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        backgroundColor: '#ffd700',
-        zIndex: 100,
-        boxShadow: '0px 2px 4px -2px rgba(0, 0, 0, 0.2)',
-    },
-    header1: {
-        paddingHorizontal: 16,
-        paddingTop: 8,
-        paddingBottom: 12,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    locationButton: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      borderRadius: 20,
-      gap: 6,
-    },
-    locationButtonIcon: {
-      borderRadius: 20,
-      backgroundColor: Colors.light,
-      padding: 10,
-    }
+  headerContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#ffd700",
+    zIndex: 100,
+    boxShadow: "0px 2px 4px -2px rgba(0, 0, 0, 0.2)",
+  },
+  header1: {
+    
+    paddingHorizontal: 16,
+    paddingTop: 8,
+    paddingBottom: 12,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  header2: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  locationText: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  locationButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 20,
+    gap: 6,
+  },
+  locationButtonIcon: {
+    borderRadius: 20,
+    backgroundColor: Colors.light,
+    padding: 10,
+  },
+  rightIcons: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  iconButton: {
+    width: 40,
+    height: 40,
+    backgroundColor: Colors.light,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  centerContent: {
+    flex: 1,
+    alignItems: "center",
+    paddingLeft: 40,
+  },
+  titleSmall: {
+    fontSize: 16,
+    fontWeight: 700,
+    marginBottom: 2,
+  },
+  locationSmall: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 2,
+  },
+  locationSmallTextt: {
+    fontSize: 12,
+    color: Colors.muted,
+  }
 });
-export default ComidasHeader
+export default ComidasHeader;
