42b8b1b8043133390aaef7919ec3694216429eb4
RamssesRM <isram0717@gmail.com>
2026-07-22 03:27:59 -0400

    Configurar expo-maps y expo-location en app.json

--- componentes/ScrollinfinitoSuave.tsx
+++ componentes/ScrollinfinitoSuave.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   ImageSourcePropType,
   StyleSheet,
