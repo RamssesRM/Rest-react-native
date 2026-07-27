90e39b44637ee21cc85e5ad0da17a34783073506
RamssesRM <isram0717@gmail.com>
2026-07-27 01:55:57 -0400

    Guardando cambios de componentes y hooks antes de sincronizar

--- componentes/ComidasList.tsx
+++ componentes/ComidasList.tsx
@@ -1,5 +1,6 @@
 import { Colors } from "@/constants/theme";
 import { useRestaurants } from "@/hooks/useRestaurants";
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import {
   ActivityIndicator,
@@ -7,6 +8,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+
 
 const ComidasList = () => {
   const { data: restaurants, isLoading, error } = useRestaurants();
@@ -23,7 +25,17 @@ const ComidasList = () => {
     <View style={styles.container}>
       {restaurants?.map((item) => (
         <View key={item.id}>
-          <TouchableOpacity style={styles.card} />
+          {/* <Link> */}
+          <TouchableOpacity style={styles.card}>
+            <Image source={item.image!} style={styles.image} />
+            <View style={styles.info}>
+          <Text style={styles.name}>{item.name}</Text>
+          <Text style={styles.descripcion} numberOfLines={2}>
+            {item.descripcion}
+          </Text>
+            </View>
+          </TouchableOpacity>
+          {/* </Link> */}
         </View>
       ))}
     </View>
@@ -34,6 +46,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  card: {
+    margin: 16,
+    borderRadius: 8,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.light,
+    overflow: "hidden", // Border la parte superior de las cars
+    boxShadow: "0px 4px 2px -2px rgba(0, 0, 0, 0.2)",
+    //elevation: 2, // eso pone feo la app, pero hay que averiguar como adaptarlo a Android
+  },
+  info:{
+    padding: 12,
+  },
+  name:{
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 4,
+  },
+  descripcion:{
+    fontSize: 14,
+    color: '#666',
+  },
   center: {
     flex: 1,
     justifyContent: "center",
@@ -42,6 +75,22 @@ const styles = StyleSheet.create({
   card: {
     // Aquí irán los estilos de tus tarjetas de comida
   },
+  metadata: {
+    borderTopColor: Colors.light,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+    padding: 10,
+  },
+  metadataText: {
+    fontSize: 13,
+    color: '#666'
+  },
+  dot: {
+    color: '#666',
+    fontSize: 13
+  }
 });
 
 export default ComidasList;
